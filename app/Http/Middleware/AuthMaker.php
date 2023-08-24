b5cef010b22acdf858d2558af5d23713f1b5d7f0
frameworkfuturistic <anshu8581@gmail.com>
2023-08-24 15:36:03 +0530

    multipart issue shorted out

--- app/Http/Middleware/AuthMaker.php
+++ app/Http/Middleware/AuthMaker.php
@@ -17,20 +17,24 @@ class AuthMaker
      */
     public function handle(Request $request, Closure $next)
     {
-        if(!Auth()->user() && $request->auth)
-        {
-            switch($request->currentAccessToken["tokenable_type"])
-            {
-                case "App\\Models\\Auth\\User": Auth::login(new \App\Models\User($request->auth));
-                                                Auth()->user()->ulb_id=$request->auth["ulb_id"];
-                                                break;
-                default                       : Auth::login(new \App\Models\ActiveCitizen($request->auth));
-                                                break;
+        if (!Auth()->user() && $request->auth) {
+            if (!is_array($request->auth)) {
+                $request->merge(["auth" => json_decode($request->auth, true)]);
             }
-            collect($request->auth)->map(function($val,$key){
+            if (!is_array($request->currentAccessToken)) {
+                $request->merge(["currentAccessToken" => json_decode($request->currentAccessToken, true)]);
+            }
+            switch ($request->currentAccessToken["tokenable_type"]) {
+                case "App\\Models\\Auth\\User":
+                    Auth::login(new \App\Models\User($request->auth));
+                    break;
+                default:
+                    Auth::login(new \App\Models\ActiveCitizen($request->auth));
+                    break;
+            }
+            collect($request->auth)->map(function ($val, $key) {
                 Auth()->user()->$key = $val;
             });
-            Auth()->user()->id=$request->auth["id"];
         }
         return $next($request);
     }
