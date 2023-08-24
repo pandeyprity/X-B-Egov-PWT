2102e3b80332594d6aed2f724bf95ef2ac0c8d1b
frameworkfuturistic <anshu8581@gmail.com>
2023-08-24 11:03:29 +0530

    search holding

--- app/Models/Property/PropProperty.php
+++ app/Models/Property/PropProperty.php
@@ -561,7 +561,7 @@ class PropProperty extends Model
             DB::raw("string_agg(prop_owners.mobile_no::VARCHAR,',') as mobile_no"),
             DB::raw("string_agg(prop_owners.owner_name,',') as owner_name"),
         )
-            ->join('ulb_ward_masters', 'ulb_ward_masters.id', 'prop_properties.ward_mstr_id')
+            ->leftjoin('ulb_ward_masters', 'ulb_ward_masters.id', 'prop_properties.ward_mstr_id')
             ->leftjoin(DB::raw("(select latitude, longitude,  prop_saf_geotag_uploads.saf_id
                                 from prop_saf_geotag_uploads 
                                 JOIN prop_properties ON prop_properties.saf_id = prop_saf_geotag_uploads.saf_id 
@@ -570,8 +570,7 @@ class PropProperty extends Model
                            ) as geotag"), function ($join) {
                 $join->on("geotag.saf_id", "=", "prop_properties.saf_id");
             })
-            ->join('prop_owners', 'prop_owners.property_id', 'prop_properties.id');
-        // ->where('prop_properties.ulb_id', $ulbId);
+            ->leftjoin('prop_owners', 'prop_owners.property_id', 'prop_properties.id');
     }
 
     /**
