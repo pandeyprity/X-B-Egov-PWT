f43372e75d34b28b9389b507c60f54bbba568a16
Arshmonster <arshadhussain82987@gmail.com>
2023-09-03 14:36:21 +0530

    arsahd

--- app/Models/Water/WaterSecondConsumer.php
+++ app/Models/Water/WaterSecondConsumer.php
@@ -172,4 +172,33 @@ class WaterSecondConsumer extends Model
             ->where('status', 1)
             ->first();
     }
+    /**
+     * 
+     */
+    public function fullWaterDetail($applicationId)
+    {
+        return WaterSecondConsumer::select(
+            'water_second_consumers.*',
+            'water_second_consumers.consumer_no',
+            'water_second_connection_charges.amount',
+            'water_second_connection_charges.charge_category',
+            'water_consumer_meters.meter_no',
+            'water_consumer_meters.connection_type',
+            'water_consumer_meters.initial_reading',
+            'water_consumer_meters.final_meter_reading',
+            'ulb_masters.ulb_name',
+            "water_consumer_owners.applicant_name",
+            "water_consumer_owners.guardian_name",
+            "water_consumer_owners.email",
+            DB::raw('ulb_ward_masters.ward_name as ward_number') // Alias the column as "ward_number"
+        ) 
+            ->join("water_consumer_owners",'water_consumer_owners.consumer_id','water_second_consumers.id')
+            ->join('ulb_masters','ulb_masters.id','water_second_consumers.ulb_id')
+            ->join('ulb_ward_masters', 'ulb_ward_masters.id', 'water_second_consumers.ward_mstr_id')
+            ->join('water_consumer_meters', 'water_consumer_meters.consumer_id', 'water_second_consumers.id')
+            ->join('water_second_connection_charges', 'water_second_connection_charges.consumer_id', 'water_second_consumers.id')
+            ->where('water_second_consumers.id', $applicationId)
+            ->where('water_second_consumers.status', 4);
+    }
+    
 }
