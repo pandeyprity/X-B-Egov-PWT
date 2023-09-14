5142e1cddab5e2888366ca5d4d4130cbd363b727
SamKerketta <samkerketta77@gmail.com>
2023-09-14 11:40:10 +0530

    doc upload

--- app/Models/Workflows/WfActiveDocument.php
+++ app/Models/Workflows/WfActiveDocument.php
@@ -35,18 +35,18 @@ class WfActiveDocument extends Model
     public function metaReqs($req)
     {
         return [
-            "active_id" => $req->activeId,
-            "workflow_id" => $req->workflowId,
-            "ulb_id" => $req->ulbId,
-            "module_id" => $req->moduleId,
-            "relative_path" => $req->relativePath,
-            "document" => $req->document,
-            "uploaded_by" => Auth()->user()->id,
-            "uploaded_by_type" => Auth()->user()->user_type,
-            "remarks" => $req->remarks ?? null,
-            "doc_code" => $req->docCode,
-            "owner_dtl_id" => $req->ownerDtlId,
-            "doc_category" => $req->docCategory ?? null
+            "active_id"         => $req->activeId,
+            "workflow_id"       => $req->workflowId,
+            "ulb_id"            => $req->ulbId,
+            "module_id"         => $req->moduleId,
+            "relative_path"     => $req->relativePath,
+            "document"          => $req->document,
+            "uploaded_by"       => authUser($req)->id,
+            "uploaded_by_type"  => authUser($req)->user_type,
+            "remarks"           => $req->remarks ?? null,
+            "doc_code"          => $req->docCode,
+            "owner_dtl_id"      => $req->ownerDtlId,
+            "doc_category"      => $req->docCategory ?? null
         ];
     }
 
