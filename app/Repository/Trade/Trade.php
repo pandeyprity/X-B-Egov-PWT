18cc305ef9e2a529bbc6f208e3d4e7edd1d3e906
sandeepBara <sandeepbara830@gmail.com>
2023-09-16 11:58:57 +0530

    ghjghj f

--- app/Repository/Trade/Trade.php
+++ app/Repository/Trade/Trade.php
@@ -3382,7 +3382,7 @@ class Trade implements ITrade
      */
     public function createApplicationNo($wardNo, $licenceId,$mShortUlbName=null)
     {
-        return ($mShortUlbName && strtoupper($mShortUlbName)!="RMC"?strtoupper($mShortUlbName)."-" : "APN") . str_pad($wardNo, 2, '0', STR_PAD_LEFT) . str_pad($licenceId, 7, '0', STR_PAD_LEFT);
+        return ($mShortUlbName && strtoupper($mShortUlbName)!="AMC"?strtoupper($mShortUlbName)."-" : "APN") . str_pad($wardNo, 2, '0', STR_PAD_LEFT) . str_pad($licenceId, 7, '0', STR_PAD_LEFT);
     }
 
     /**
