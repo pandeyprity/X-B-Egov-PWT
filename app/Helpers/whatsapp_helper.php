c709257604af1a0b3757f1ac07d496483315c5c2
Arshmonster <arshadhussain82987@gmail.com>
2023-09-29 10:36:57 +0530

    changes

--- app/Helpers/whatsapp_helper.php
+++ app/Helpers/whatsapp_helper.php
@@ -2,29 +2,21 @@
 
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Http;
-
-if (!function_exists('WHATSAPPJHGOVT')) {
-    function WHATSAPPJHGOVT($mobileno, $templateid, array $message = [],)
+if(!function_exists('WHATSAPPJHGOVT'))
+{
+    function WHATSAPPJHGOVT($mobileno,$templateid,array $message=[])
     {
         $bearerToken = Config::get("NoticeConstaint.WHATSAPP_TOKEN");
         $numberId = Config::get("NoticeConstaint.WHATSAPP_NUMBER_ID");
-        $url = Config::get("NoticeConstaint.WHATSAPP_URL");
-
-
         $url = Config::get("NoticeConstaint.WHATSAPP_URL");  
         $data = [
             "messaging_product" => "whatsapp",
             "recipient_type" => "individual",
-            "to" => "+91$mobileno",                             //<--------------------- here
+            "to" => "+91$mobileno", //<--------------------- here
             "type" => "template",
             "template" => [
                 "name" => "$templateid",
                 "language" => [
-                    "code" => "en_US"                           //<-------------------- en:English/ en_US:EngPDf
-                ],
-                "components" => [
-                    ($message
-                        ?
                     "code" => "en_US"
                 ],
                 "components" => [
@@ -33,25 +25,13 @@ if (!function_exists('WHATSAPPJHGOVT')) {
                     (
                         ($message['content_type']??"")=="pdf"?
                         (
-                            ($message['content_type'] ?? "") == "pdf"
-                            ?
-                            ([
+                            [
                                 "type" => "header",
                                 "parameters" => [
                                     [
                                         "type" => "document",
                                         "document" => $message[0]
                                         // [
-<<<<<<< HEAD
-                                        //     "link" => "http://www.xmlpdf.com/manualfiles/hello-world.pdf",
-                                        //     "filename" => "Payment Receipt.pdf"
-                                        // ]
-                                    ]
-                                ]
-                            ])
-                            : (($message['content_type'] ?? "") == "text" ?
-                                ([
-=======
                                         //     "link"=> "http://www.xmlpdf.com/manualfiles/hello-world.pdf",
                                         //     "filename"=> "Payment Receipt.pdf"
                                         // //     // $message[0]
@@ -65,33 +45,20 @@ if (!function_exists('WHATSAPPJHGOVT')) {
                             ($message['content_type']??"")=="text"?
                             (
                                 [
->>>>>>> 87f2b272b9e1aa17d22d42dd943feb7ac2f899d7
                                     "type" => "body",
-                                    "parameters" => array_map(function ($val) {
-                                        return ["type" => "text", "text" => $val];
-                                    }, $message[0] ?? [])
+                                    "parameters" => array_map(function($val){
+                                        return ["type"=>"text","text"=>$val];
+                                    },$message[0]??[])
                                 ]
-                                )
-                                :
-                                "")
+                            )
+                            :
+                            ""
                         )
-                        : ""),
+
+                    )
+                    :""),
                 ]
             ]
-<<<<<<< HEAD
-        ];
-        $result = Http::withHeaders([
-
-            "Authorization" => "Bearer $bearerToken",
-            "contentType" => "application/json"
-
-        ])->post($url . $numberId . "/messages", $data);
-        $responseBody = json_decode($result->getBody(), true);
-        if (isset($responseBody["error"])) {
-            $response = ['response' => false, 'status' => 'failure', 'msg' => $responseBody];
-        } else {
-            $response = ['response' => true, 'status' => 'success', 'msg' => $responseBody];
-=======
         ];       
         $result = Http::withHeaders([
     
@@ -103,18 +70,22 @@ if (!function_exists('WHATSAPPJHGOVT')) {
         if (isset($responseBody["error"]))
         {
             $response = ['response'=>false, 'status'=> 'failure', 'msg'=>$responseBody];
->>>>>>> 87f2b272b9e1aa17d22d42dd943feb7ac2f899d7
         }
-
+        else
+        {
+            $response = ['response'=>true, 'status'=> 'success', 'msg'=>$responseBody];                
+        }
+        
         return $response;
     }
 }
 
-if (!function_exists('Whatsapp_Send')) {
-    function Whatsapp_Send($mobileno, $templateid, array $message = [], $lang = "en")
+if(!function_exists('Whatsapp_Send'))
+{
+    function Whatsapp_Send($mobileno,$templateid,array $message=[])
     {
         // $res=WHATSAPPJHGOVT("9708846652", $templateid,$message);
-        $res = WHATSAPPJHGOVT($mobileno, $templateid, $message, $lang);
+        $res=WHATSAPPJHGOVT($mobileno, $templateid,$message);
         return $res;
     }
 }
