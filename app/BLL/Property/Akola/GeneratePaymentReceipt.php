175d342f376257dd64a4e73c2ba245a0db59e2ea
frameworkfuturistic <anshu8581@gmail.com>
2023-10-03 20:18:13 +0530

    Payment Receipt

--- app/BLL/Property/Akola/GeneratePaymentReceipt.php
+++ app/BLL/Property/Akola/GeneratePaymentReceipt.php
@@ -178,6 +178,7 @@ class GeneratePaymentReceipt
             "verifyStatus" => $this->_trans->verify_status,                     // (0-Not Verified,1-Verified,2-Under Verification,3-Bounce)
             "applicationNo" => $this->_propertyDtls->application_no,
             "customerName" => $this->_propertyDtls->applicant_name,
+            "ownerName" => $this->_propertyDtls->owner_name,
             "guardianName" => $this->_propertyDtls->guardian_name,
             "mobileNo" => $this->_propertyDtls->mobile_no,
             "address" => $this->_propertyDtls->prop_address,
