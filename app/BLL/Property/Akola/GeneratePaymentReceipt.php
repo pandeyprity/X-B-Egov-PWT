e38152855c79ee29f512c2f2d42342d71184fe37
frameworkfuturistic <anshu8581@gmail.com>
2023-10-04 14:14:01 +0530

    test

--- app/BLL/Property/Akola/GeneratePaymentReceipt.php
+++ app/BLL/Property/Akola/GeneratePaymentReceipt.php
@@ -178,9 +178,9 @@ class GeneratePaymentReceipt
             "transactionTime" => $this->_trans->created_at->format('H:i'),
             "verifyStatus" => $this->_trans->verify_status,                     // (0-Not Verified,1-Verified,2-Under Verification,3-Bounce)
             "applicationNo" => $this->_propertyDtls->application_no,
-            "customerName" => !isEmptyString($this->_propertyDtls->applicant_name) ? $this->_propertyDtls->applicant_name : $this->_propertyDtls->applicant_marathi,
-            "ownerName" => !isEmptyString($this->_propertyDtls->owner_name) ? $this->_propertyDtls->owner_name : $this->_propertyDtls->owner_name_marathi,
-            "guardianName" => !isEmptyString($this->_propertyDtls->guardian_name) ? $this->_propertyDtls->guardian_name : $this->_propertyDtls->guardian_name_marathi,
+            "customerName" => !empty($this->_propertyDtls->applicant_name) ? $this->_propertyDtls->applicant_name : $this->_propertyDtls->applicant_marathi,
+            "ownerName" => !empty($this->_propertyDtls->owner_name) ? $this->_propertyDtls->owner_name : $this->_propertyDtls->owner_name_marathi,
+            "guardianName" => !empty($this->_propertyDtls->guardian_name) ? $this->_propertyDtls->guardian_name : $this->_propertyDtls->guardian_name_marathi,
             "mobileNo" => $this->_propertyDtls->mobile_no,
             "address" => $this->_propertyDtls->prop_address,
             "zone_name" => $this->_propertyDtls->zone_name,
