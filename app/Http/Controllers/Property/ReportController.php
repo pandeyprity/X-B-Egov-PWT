47ed65a764b99669435ee2460cb6398a17b53abe
sandeepBara <sandeepbara830@gmail.com>
2023-11-06 12:07:56 +0530

    fdgds

--- app/Http/Controllers/Property/ReportController.php
+++ app/Http/Controllers/Property/ReportController.php
@@ -622,6 +622,28 @@ class ReportController extends Controller
         return $this->Repository->paymentModedealyCollectionRptV1($request);
     }
 
+    public function individualDedealyCollectionRptV1(Request $request)
+    {
+        $validated = Validator::make(
+            $request->all(),
+            [
+                "fromDate" => "nullable|date|date_format:Y-m-d",
+                "uptoDate" => "nullable|date|date_format:Y-m-d",
+                "wardId" =>"nullable|digits_between:1,9223372036854775807",
+                "zoneId"  =>"nullable|digits_between:1,9223",
+                "userId" =>"nullable|digits_between:1,9223",
+            ]
+        );
+        if ($validated->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'validation error',
+                'errors' => $validated->errors()
+            ]);
+        }
+        return $this->Repository->individualDedealyCollectionRptV1($request);
+    }
+
     public function mplReport2(Request $request)
     {
         try {
